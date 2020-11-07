4f86deec055f3759f4de93a81d528821c1fe6c0d
wa1tnr <wa1tnr@users.noreply.github.com>
2020-11-07 11:34:20 +0000

    empty sketch in CPP file

---
+++ cbuf-proj-a/buff-sketch-a.cpp
@@ -0,0 +1,10 @@
+// Sat Nov  7 11:24:45 UTC 2020
+// target mcu to be determined
+// probably CircuitPlayground Express from Adafruit
+// aka 'the CPX' board
+#include <Arduino.h>
+
+void setup(void) { }
+void loop(void) { }
+
+// END.
