1d911ab2405c015498534da1bc43fdab5bd32d86
wa1tnr <wa1tnr@users.noreply.github.com>
2020-11-07 11:54:04 +0000

    LED support D13 added

--- cbuf-proj-a/buff-sketch-a.cpp
+++ cbuf-proj-a/buff-sketch-a.cpp
@@ -5,16 +5,29 @@
 
 #include <Arduino.h>
 
+    // LED D13 support
+void init_led(void) {
+    pinMode(13,1); // PA17 CPX
+    digitalWrite(13,1);
+    delay(3333);
+    digitalWrite(13,0);
+    delay(3333);
+}
+
 void setup(void) {
     Serial.begin(115200);
     while (!Serial); // wait
     delay(9999); // 9.9 seconds
     Serial.println("Hello from the cbuf project a - 7 Nov 2020.");
     Serial.println("\n functional program - validated. 11:39z\n");
+
+    init_led(); // blink once
+
     for (int i=2222; i>0; i--) { // countdown
         delay(333);
         Serial.print(" mark ");
     }
+
 }
 void loop(void) {
     // nothing
