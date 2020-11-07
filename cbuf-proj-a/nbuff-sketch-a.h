4cefd886c666224a93eefd0806f67586c6690f88
wa1tnr <wa1tnr@users.noreply.github.com>
2020-11-07 12:29:28 +0000

    RAPID_PROTO test cpp

--- cbuf-proj-a/nbuff-sketch-a.h
+++ cbuf-proj-a/nbuff-sketch-a.h
@@ -4,7 +4,12 @@
 // aka 'the CPX' board
 
 #define SLOW_VAL 3
+#ifdef RAPID_PROTO
+#define GUARD_TIME 123 // milliseconds
+#endif
+#ifndef RAPID_PROTO
 #define GUARD_TIME 12333 // milliseconds
+#endif
 #define LED 13
 #define blink_ON()  digitalWrite(LED,1)
 #define blink_OFF() digitalWrite(LED,0)
