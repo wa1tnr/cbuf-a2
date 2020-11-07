24ef4b30a767b240a150610ce463db41136ceb6d
wa1tnr <wa1tnr@users.noreply.github.com>
2020-11-07 11:40:52 +0000

    basic serial print based loop sketch

--- cbuf-proj-a/buff-sketch-a.cpp
+++ cbuf-proj-a/buff-sketch-a.cpp
@@ -2,9 +2,22 @@
 // target mcu to be determined
 // probably CircuitPlayground Express from Adafruit
 // aka 'the CPX' board
+
 #include <Arduino.h>
 
-void setup(void) { }
-void loop(void) { }
+void setup(void) {
+    Serial.begin(115200);
+    while (!Serial); // wait
+    delay(9999); // 9.9 seconds
+    Serial.println("Hello from the cbuf project a - 7 Nov 2020.");
+    Serial.println("\n functional program - validated. 11:39z\n");
+    for (int i=2222; i>0; i--) { // countdown
+        delay(333);
+        Serial.print(" mark ");
+    }
+}
+void loop(void) {
+    // nothing
+}
 
 // END.
