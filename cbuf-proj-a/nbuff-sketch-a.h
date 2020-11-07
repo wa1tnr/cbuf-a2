dde4a7698d46ac8e077d579bda499b62a6c19991
wa1tnr <wa1tnr@users.noreply.github.com>
2020-11-07 12:26:10 +0000

    put macros in include file

---
+++ cbuf-proj-a/nbuff-sketch-a.h
@@ -0,0 +1,10 @@
+// Sat Nov  7 11:24:45 UTC 2020
+// target mcu to be determined
+// probably CircuitPlayground Express from Adafruit
+// aka 'the CPX' board
+
+#define SLOW_VAL 3
+#define GUARD_TIME 12333 // milliseconds
+#define LED 13
+#define blink_ON()  digitalWrite(LED,1)
+#define blink_OFF() digitalWrite(LED,0)
